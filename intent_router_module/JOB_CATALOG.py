220fd1e4000a943995eaf477d3760c3fe07d78c6
benjaestupinan <benja.3stupinan@gmail.com>
2026-02-16 20:19:51 -0300

    feat(JOB_CATALOG.py): read the JOB_CATALOG.json instead of having it duplicated

--- intent_router_module/JOB_CATALOG.py
+++ intent_router_module/JOB_CATALOG.py
@@ -1,21 +1,4 @@
-JOB_CATALOG = {
-    "echo": {
-        "job_id": "echo",
-        "description": "Devuelve exactamente el mensaje entregado, sin modificaciones.",
-        "parameters": {
-            "message": {"type": "string", "description": "Texto a devolver tal cual."}
-        },
-    },
+import json
 
-    "get_system_date_and_time": {
-        "job_id": "get_system_date_and_time",
-        "description": "Obtiene la fecha y hora actual del sistema.",
-        "parameters": {},
-    },
-    #
-    # "delay_job": {
-    #     "job_id": "delay_job",
-    #     "description": "Ejecuta otro job después de un retraso especificado.",
-    #     "parameters": {"delay_seconds": {"type": "number"}, "job": {"type": "job"}},
-    # },
-}
+with open("./JOB_CATALOG.json") as j:
+    JOB_CATALOG = json.load(j)
